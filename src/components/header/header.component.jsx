5e4e8e0a5f4b89512a89cbb5a0cbee081d599978
sWlodarczyk <rahluu@gmail.com>
2019-10-06 11:05:05 +0200

    updated shop data with new data, updated cart utils to group items in cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -29,7 +29,8 @@ const Header = ({ currentUser, hidden }) => (
                 )}
             <CartIcon/>
         </div>
-        {hidden ? null : <CartDropdown />
+        { 
+            hidden ? null : <CartDropdown />
         }
     </div>
 );
