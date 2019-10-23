cde424fef2000a40a481ea3652e4193433caa27d
sWlodarczyk <rahluu@gmail.com>
2019-10-23 18:29:30 +0200

    added addItem and removeItem functionality to checkout item component

--- src/redux/user/user.selectors.js
+++ src/redux/user/user.selectors.js
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect';
-import cartDropdownComponent from '../../components/cart-dropdown/cart-dropdown.component';
+
 
 const selectUser = state => state.user;
 
